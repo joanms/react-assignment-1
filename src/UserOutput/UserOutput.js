f12f18f2af24f5eb1ea20aaca76b2a1ee4497add
Joan Sweeney <joanms@eircom.net>
2020-07-01 14:26:46 +0100

    Initial commit

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Nam eget mauris eget erat consectetur scelerisque. 
+                Proin id urna at dolor congue faucibus id ac tortor. 
+                Sed a quam eget dolor consequat varius ut vitae turpis. 
+                Aenean dui eros, consectetur eu tristique sit amet, 
+                scelerisque at ipsum. Vestibulum tincidunt accumsan 
+                velit quis finibus. Integer in ullamcorper metus. 
+                Nunc in diam mi.
+            </p>
+            <p>
+                Vivamus aliquam mollis leo a vestibulum. Pellentesque euismod 
+                varius elit vitae viverra. Donec eget nisl eget nunc imperdiet 
+                auctor ac ac sem. Duis ac euismod dolor. Suspendisse a neque mollis, 
+                condimentum ante a, vehicula libero. Ut maximus convallis erat, 
+                quis fermentum sapien mollis nec. Pellentesque lacinia libero ac ante 
+                tristique blandit. Vivamus eu nisi rutrum, imperdiet velit sit amet, 
+                aliquam odio. Nunc a nibh sed diam pretium malesuada nec id justo.
+            </p>
+        </div>
+    )
+}
