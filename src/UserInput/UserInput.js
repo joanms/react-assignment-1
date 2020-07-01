f554a6ad1f83dab82a114ee69088ebbe4b9dff57
Joan Sweeney <joanms@eircom.net>
2020-07-01 15:31:42 +0100

    Add styles

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const userInput = (props) => {
+    const style = {
+        border: '4px solid blue'
+    }
     return <input
         type="text"
+        style={style}
         onChange={props.changed}
         value={props.currentName} />;
 };
